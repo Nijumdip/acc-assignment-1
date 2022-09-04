1087f669411e8a0fc6ea35f0f9dae86a441acc02
Nijumdip <nijumdip019911@gmail.com>
2022-09-05 02:15:45 +0600

    added save a user route

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -2,11 +2,9 @@ const fs = require("fs");
 // const data = fs.readFileSync('userData.json');
 const data = JSON.parse(fs.readFileSync("userData.json"));
 
-
-
 module.exports.getRandomUser = (req, res, next) => {
-    // res.send("Random User Found");
-/*  const randomValue = Math.floor(Math.random() * data.length)
+  // res.send("Random User Found");
+  /*  const randomValue = Math.floor(Math.random() * data.length)
         console.log(randomValue);
         res.status(200).json({
             message: {
@@ -14,37 +12,75 @@ module.exports.getRandomUser = (req, res, next) => {
                 message: "Random user found.",
                 user:data[randomValue]
         } */
-    const randomValue = data[parseInt(Math.random() * data.length)];
-        // console.log(randomValue);
-        res.status(200).send({
-            message: {
-                success: true,
-                message: "Random user found.",
-                user: randomValue
-        },
-    });
+  const randomValue = data[parseInt(Math.random() * data.length)];
+  // console.log(randomValue);
+  res.status(200).send({
+    message: {
+      success: true,
+      message: "Random user found.",
+      user: randomValue,
+    },
+  });
 };
 
-
 module.exports.getAllUser = (req, res, next) => {
-  // res.send('All User Found');
-    const { limit } = req.query;
-    // console.log(limit);
-    // res.send(data.slice(0,limit));
+  // res.send(data);
+  const { limit } = req.query;
+  // console.log(limit);
+  // res.send(data.slice(0,limit));
+  // res.send(data.slice(0, parseInt(limit)));
+  if (limit) {
     res.status(200).send({
-        message: {
-            success: true,
-            message: "Limited Users",
-            // limitedUsers: (data.slice(0,limit))
-            limitedUsers: (data.slice(0, parseInt(limit)))
-    },
-});
-    
+      message: {
+        success: true,
+        message: "Limited Users",
+        // limitedUsers: (data.slice(0,limit))
+        limitedUsers: data.slice(0, parseInt(limit)),
+      },
+    });
+  }
+  else {
+    res.status(200).send({
+      message: {
+        success: true,
+        message: "All Users Found",
+        // limitedUsers: (data.slice(0,limit))
+        users: data
+      },
+    });
+  }
 };
 
 module.exports.saveUser = (req, res, next) => {
-  res.send("Post A User");
+  // res.send("Saved A User");
+  // console.log(req.body);
+  const { gender, name, contact, address, photoUrl } = req.body;
+  // console.log(gender);
+  // const randomId = Math.floor(Math.random() * (50-10)+1);
+  // console.log(randomId);
+  // data.push({id:randomId, ...req.body});
+  data.push({ id: data.length + 1, ...req.body });
+  fs.writeFileSync("userData.json", JSON.stringify(data))
+  if (req.body !== gender, name, contact, address, photoUrl) {
+    res.status(500).send({
+      message: {
+        success: false,
+        message: "Please provide a valid data",
+      },
+    });
+  }
+  else {
+    res.status(200).send({
+      message: {
+        success: true,
+        message: "User added successfully",
+        users:data
+      },
+    });
+  }
 };
+
+
 
 module.exports.updateRandomUser = (req, res, next) => {
   // res.send('Update A Random User')
--- routes/user.route.js
+++ routes/user.route.js
@@ -2,12 +2,6 @@ const express = require('express');
 const userControllers = require('../controllers/user.controller');
 const router = express.Router();
 
-/* router.route("/").all((req, res) => {
-    res.json({
-        body: "user route",
-    })
-}); */
-
 
 router.route('/random').get(userControllers.getRandomUser);
 
